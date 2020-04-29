facdee0255afba020afeb9c05b93555710030002
Aiden Fertich <greyfertich@gmail.com>
2020-04-29 15:28:07 -0400

    Updated gitignore

---
+++ twitter_credentials.py
@@ -0,0 +1,6 @@
+# Variables that contain the user credentials to access Twitter API.
+# Fill in variables with your own Twitter API Keys
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = "" 
+CONSUMER_KEY = ""
+CONSUMER_SECRET = "" 
