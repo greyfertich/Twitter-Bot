22d6aabe859effdb97f69a8e8a8092a3f1760b7c
Aiden Fertich <greyfertich@gmail.com>
2020-04-29 15:33:19 -0400

    remove file from vs

--- twitter_credentials.py
+++
@@ -1,6 +0,0 @@
-# Variables that contain the user credentials to access Twitter API.
-# Fill in variables with your own Twitter API Keys
-ACCESS_TOKEN = ""
-ACCESS_TOKEN_SECRET = "" 
-CONSUMER_KEY = ""
-CONSUMER_SECRET = "" 
